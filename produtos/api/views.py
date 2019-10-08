076cc8eb81f7a90435089d90bda9e009b4240a1d
Nikolas <nikolasbrandao@gmail.com>
2019-10-07 23:09:18 -0300

    Adição de resposta em xml

--- produtos/api/views.py
+++ produtos/api/views.py
@@ -2,8 +2,11 @@ from ..models import Produto
 from . import serializers
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework_xml.renderers import XMLRenderer
 
 class ProdutoListView(generics.ListAPIView):
+    renderer_classes = (JSONRenderer, XMLRenderer)
     queryset = Produto.objects.all()
     serializer_class = serializers.ProdutoSerializer
 
