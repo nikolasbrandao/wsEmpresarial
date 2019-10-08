2053d9d427df11c0b89b7b63b0bf41d8f30dc384
Nikolas <nikolasbrandao@gmail.com>
2019-10-07 22:15:00 -0300

    adição de modelo de Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    codigo = models.IntegerField()
+    descricao = models.TextField()
+    preco = models.DecimalField()
+
+    def __str__(self):
+        return self.name
